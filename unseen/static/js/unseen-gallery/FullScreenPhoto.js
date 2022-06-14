00b343f7a1af9a9bd3673e45838c928b7def0d3c
Althea Solis <althea@cca.edu>
2022-06-14 11:48:08 -0700

    Gallery photo page style

--- unseen/static/js/unseen-gallery/FullScreenPhoto.js
+++ unseen/static/js/unseen-gallery/FullScreenPhoto.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.xsrfCookieName = "csrftoken";
@@ -14,7 +15,7 @@ function FullScreenPhoto(props) {
 
   useEffect(() => {
     axios
-      .get(`/gallery/api/${slug}`)
+      .get(`/gallery/api/${slug}/`)
       .then((response) => setPhoto(response.data))
       .catch((err) => setError(err))
       .finally(() => setLoaded(true));
@@ -22,22 +23,40 @@ function FullScreenPhoto(props) {
 
   return (
     <div className="fs-photo">
-      {!loaded ? (
-        <p>loading...</p>
-      ) : (
-        <div>
-          <img className="fs-photo__image" src={`${photo.photo}`} />
+      {!loaded && <p>loading...</p>}
+      <section className="fs-photo__control-wrapper">
+        <Link className="link--unstyled" to={`/gallery/`}>
+          <button className="control-button control-button--fs control-button--close-fs">
+            <span className="icon icon--control-button icon--close-fs" />
+            <span className="control-button__text">Back</span>
+          </button>
+        </Link>
+
+        <div className="fs-photo__controls">
+          <button className="control-button control-button--fs control-button--previous-fs">
+            <span className="icon icon--control-button icon--previous-fs" />
+            <span className="control-button__text">Previous</span>
+          </button>
+          <button className="control-button control-button--fs control-button--next-fs">
+            <span className="control-button__text">Next</span>
+            <span className="icon icon--control-button icon--next icon--next-fs" />
+          </button>
+        </div>
+      </section>
+      <div className="fs-photo__wrapper">
+        <img className="fs-photo__image" src={`${photo.photo}`} />
+        <div className="fs-photo__info-wrapper">
+          <a href={`/gallery/${photo.slug}/conversation/`}>
+            <button className="button button--fs">View Responses</button>
+          </a>
           <div className="fs-photo__info">
             <h2 className="fs-photo__title">{photo.name}</h2>
             {photo.description && (
               <p className="fs-photo__description">{photo.description}</p>
             )}
-            {photo.url && (
-                <p className="fs-photo__description">See Full Work</p>
-            )}
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
